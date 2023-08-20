4dbfd8b291ba000d7d193fc4e2964a46b27ba2b4
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-08-20 21:19:24 +0800

    News Feature
    Added Mobile layout & project design theme, and implemented News Feature with SwiperJS

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    loop: true
+  });
