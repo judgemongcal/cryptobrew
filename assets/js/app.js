aa1c2f35d170c94656f46700381a9c6d83e496bd
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-08-21 17:05:04 +0800

    Fetching Data from NYTimes API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+// SwiperJS
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -8,3 +10,15 @@ var swiper = new Swiper(".mySwiper", {
     },
     loop: true
   });
+
+  
+  // News API (NY Times)
+  const newsAPIkey = 'Mt0V4THOHF3RqwmodajlbaijTAdVUS2r';
+
+  const getNews = async (newsAPIKey) => {
+    const data = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=crypto&api-key=${newsAPIKey}`);
+    const result = await data.json();
+    console.log(result);
+};
+
+  getNews(newsAPIkey);
