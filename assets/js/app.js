dbfcbe3267620bf731ce9fe6f80cbf1258f48008
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-08-23 23:20:44 +0800

    Added Router

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,32 @@
 const global = {
-  currentPage: window.location.pathname,
+  currentPath: window.location.pathname,
+  currentPage: 0,
   newsApi: {
     apiKey: 'Mt0V4THOHF3RqwmodajlbaijTAdVUS2r'
   }
 };
 
 
-const newsSlide = document.querySelector('.swiper-wrapper');
+const indexNewsSlide = document.querySelector('.swiper-wrapper');
+const newsPageCard = document.querySelector('.news-container');
 
-// NEWS FEATURE
+// Fetch News API
 const newsAPIkey = 'Mt0V4THOHF3RqwmodajlbaijTAdVUS2r'; /* NY Times API */
 
 const getNews = async () => {
-  const data = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=crypto-currency&sort=newest&api-key=${global.newsApi.apiKey}`);
+  const data = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=crypto-currency&sort=newest&api-key=${global.newsApi.apiKey}&page=${global.currentPage}`);
   const result = await data.json();
   console.log(result);
   return result;
 };
 
 
+// Display News
+
 const displayNews = async () => {
   const res = await getNews();
   console.log(res);
-  for(let i = 0; i < 10; i++){
+  for(let i = 0; i < res.response.docs.length; i++){
     const source = res.response.docs;
     const givenDate = source[i].pub_date;
     const date = new Date(givenDate);
@@ -37,27 +41,66 @@ const displayNews = async () => {
     const finalDate = formater.format(date);
 
     const div = document.createElement('div');
-    div.classList.add('swiper-slide');
-    div.innerHTML = `
-    <div class="swiper-slide">
-    ${
-      source[i].multimedia[22]? `<img src="https://www.nytimes.com/${source[i].multimedia[22].url}" alt="">` :
-      `<img src="./assets/images/image_pholder.webp" alt="">`
-    }
-    <div class="news-details">
-      <h2 class="news-title">${source[i].headline.main}</h2>
-      <p id="news-date" class="heavy">${finalDate}</p>
-      <p id="news-date" class="heavy">${source[i].source}</p>
-      <p id="news-summary">${source[i].abstract}</p>
-  </div>
-    `;
 
-    newsSlide.appendChild(div);
+    switch(global.currentPath){
+      case '/index.html':
+        div.classList.add('swiper-slide');
+        div.innerHTML = `
+        <div class="swiper-slide">
+        ${
+          source[i].multimedia[22]? `<img src="https://www.nytimes.com/${source[i].multimedia[22].url}" alt="">` :
+          `<img src="./assets/images/image_pholder.webp" alt="">`
+        }
+        <div class="news-details">
+          <h2 class="news-title">${source[i].headline.main}</h2>
+          <p id="news-date" class="heavy">${finalDate}</p>
+          <p id="news-date" class="heavy">${source[i].source}</p>
+          <p id="news-summary">${source[i].abstract}</p>
+      </div>
+        `;
+    
+        indexNewsSlide.appendChild(div);
+        break;
+
+      case '/news.html':
+        div.classList.add('news-card', 'shadow-1');
+        div.innerHTML = `
+          <div class="news-details">
+              ${source[i].multimedia[22]? `<img src="https://www.nytimes.com/${source[i].multimedia[22].url}" alt="">` :
+          `<img src="./assets/images/image_pholder.webp" alt="">`}
+              <h2 class="news-title">${source[i].headline.main}</h2>
+              <p id="news-date" class="heavy">${finalDate}</p>
+              <p id="news-date" class="heavy">${source[i].source}</p>
+              <p id="news-summary">${source[i].abstract}</p>
+          </div>
+        `;
+        newsPageCard.appendChild(div);
+        break;
+    };
+
+   
+    
 
   }
 }
 
-  displayNews();
+
+
+  
+
+
+// Router
+const init = () => {
+  console.log(global.currentPath);
+  switch(global.currentPath) {
+    case '/news.html': 
+      displayNews();
+      break;
+    case '/index.html': 
+      displayNews();
+      break;
+  }
+}
 
 
 
@@ -67,11 +110,7 @@ const displayNews = async () => {
 
 
 
-
-
-
-
-
+init();
 
 
 
