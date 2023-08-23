63c8e9f541ddbb6f5c708ea5bd45262b5d73b119
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-08-23 23:45:22 +0800

    Added Sort Feature (Newest to Oldest and vice versa)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,32 @@
+const indexNewsSlide = document.querySelector('.swiper-wrapper');
+const newsPageContainer = document.querySelector('.news-container');
+const newsNewestBtn = document.querySelector('.newest');
+const newsOldestBtn = document.querySelector('.oldest');
+const newsNextBtn = document.querySelector('.next');
+const newsPrevBtn = document.querySelector('.previous');
+const today = new Date();
+
 const global = {
   currentPath: window.location.pathname,
   currentPage: 0,
   sorting: 'newest',
+  startDate: '2022-01-01', 
+  endDate: today.toISOString().split('T')[0],
   newsApi: {
     apiKey: 'Mt0V4THOHF3RqwmodajlbaijTAdVUS2r'
   }
 };
 
-const indexNewsSlide = document.querySelector('.swiper-wrapper');
-const newsPageCard = document.querySelector('.news-container');
-const newsNewestBtn = document.querySelector('.newest');
-const newsOldestBtn = document.querySelector('.oldest');
-const newsNextBtn = document.querySelector('.next');
-const newsPrevBtn = document.querySelector('.previous');
 
+
+
+
+console.log(today);
 // Fetch News API
 const newsAPIkey = 'Mt0V4THOHF3RqwmodajlbaijTAdVUS2r'; /* NY Times API */
 
 const getNews = async () => {
-  const data = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=crypto-currency&sort=${global.sorting}&api-key=${global.newsApi.apiKey}&page=${global.currentPage}`);
+  const data = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=crypto-currency&sort=${global.sorting}&api-key=${global.newsApi.apiKey}&begin_date=${global.startDate}&end_date=${global.endDate}&page=${global.currentPage}`);
   const result = await data.json();
   console.log(result);
   return result;
@@ -27,8 +35,8 @@ const getNews = async () => {
 
 // Display News
 
-const displayNews = async () => {
-  const res = await getNews();
+const displayNews = async (result) => {
+  const res = await result;
   console.log(res);
   for(let i = 0; i < res.response.docs.length; i++){
     const source = res.response.docs;
@@ -78,16 +86,33 @@ const displayNews = async () => {
               <p id="news-summary">${source[i].abstract}</p>
           </div>
         `;
-        newsPageCard.appendChild(div);
+        newsPageContainer.appendChild(div);
         break;
     };
 
-   
-    
-
   }
+};
+
+// Reset News Page Content
+
+const resetNews = () => {
+  newsPageContainer.innerHTML = '';
 }
 
+// Filter News
+const newestFirst = async () => {
+  global.sorting = 'newest';
+  const res = await getNews();
+  resetNews();
+  displayNews(res);
+}
+
+const oldestFirst = async () => {
+  global.sorting = 'oldest';
+  const res = await getNews();
+  resetNews();
+  displayNews(res);
+}
 
 
   
@@ -98,10 +123,12 @@ const init = () => {
   console.log(global.currentPath);
   switch(global.currentPath) {
     case '/news.html': 
-      displayNews();
+      displayNews(getNews());
+      newsNewestBtn.addEventListener('click', newestFirst);
+      newsOldestBtn.addEventListener('click', oldestFirst);
       break;
     case '/index.html': 
-      displayNews();
+      displayNews(getNews());
       break;
   }
 }
